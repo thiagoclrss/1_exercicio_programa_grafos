f78e7dff33200f20b738f1dde64e92c3e52c1927
thiagoclrss <thiagoclrss77@gmail.com>
2024-12-14 04:51:42 -0300

    kruskal algorithm with union find

--- ep1.c
+++ ep1.c
@@ -12,23 +12,89 @@ struct aresta {
     int custo;
 };
 
+struct unionFind {
+    int *pai;      
+    int *rank;     
+};
+
+void inicializarUnionFind(struct unionFind *uf, int n) {
+    uf->pai = malloc(n * sizeof(int));
+    uf->rank = malloc(n * sizeof(int));
+    for (int i = 0; i < n; i++) {
+        uf->pai[i] = i; 
+        uf->rank[i] = 0; 
+    }
+}
+
+int encontrar(struct unionFind *uf, int x) {
+    if (uf->pai[x] != x) {
+        uf->pai[x] = encontrar(uf, uf->pai[x]); 
+    }
+    return uf->pai[x];
+}
+
+void unir(struct unionFind *uf, int x, int y) {
+    int raizX = encontrar(uf, x);
+    int raizY = encontrar(uf, y);
+    if (raizX != raizY) {
+        if (uf->rank[raizX] < uf->rank[raizY]) {
+            uf->pai[raizX] = raizY;
+        } else if (uf->rank[raizX] > uf->rank[raizY]) {
+            uf->pai[raizY] = raizX;
+        } else {
+            uf->pai[raizY] = raizX;
+            uf->rank[raizX]++;
+        }
+    }
+}
+
+void liberarUnionFind(struct unionFind *uf) {
+    free(uf->pai);
+    free(uf->rank);
+}
+
 int compararPorCusto(const void *a, const void *b) {
     struct aresta *arestaA = (struct aresta *)a;
     struct aresta *arestaB = (struct aresta *)b;
     return arestaA->custo - arestaB->custo;
 }
 
-void kruskal(int quantidadeDeVertices, int quantidadeDeArestas, struct aresta *listaDeArestas){
-    int custoTotal;
+void kruskal(int quantidadeDeVertices, int quantidadeDeArestas, struct aresta *grafo){
+    int custoTotal = 0;
+    int agmIndex = 0;
+    struct aresta *arvoreGeradoraMinima = malloc(sizeof(struct aresta) * (quantidadeDeVertices - 1));
 
-    //printf("quantidade de vertices e quantidade de arestas: %d %d\n", quantidadeDeVertices, quantidadeDeArestas);
+    qsort(grafo, quantidadeDeArestas, sizeof(struct aresta), compararPorCusto);
     
-    qsort(listaDeArestas, quantidadeDeArestas, sizeof(struct aresta), compararPorCusto);
-    
-    for (int i = 0; i < quantidadeDeArestas; i++) {
-        printf("Aresta %d: Origem = %d, Destino = %d, Custo = %d\n",
-               i, listaDeArestas[i].origem, listaDeArestas[i].destino, listaDeArestas[i].custo);
+    struct unionFind uf;
+    inicializarUnionFind(&uf, quantidadeDeVertices);
+
+    //construindo a agm
+    for (int i = 0; i < quantidadeDeArestas && agmIndex < quantidadeDeVertices - 1; i++) {
+        struct aresta arestaAtual = grafo[i];
+
+        // Verifica se a aresta atual forma um ciclo
+        if (encontrar(&uf, arestaAtual.origem - 1) != encontrar(&uf, arestaAtual.destino - 1)) {
+            arvoreGeradoraMinima[agmIndex++] = arestaAtual; // Adiciona a aresta à MST
+            custoTotal += arestaAtual.custo;
+            unir(&uf, arestaAtual.origem - 1, arestaAtual.destino - 1);
+        }
     }
+    
+    
+    printf("Árvore geradora de custo mínimo:\n");
+    for (int i = 0; i < agmIndex; i++) {
+        printf("Origem: %d, Destino: %d, Custo: %d\n", 
+        arvoreGeradoraMinima[i].origem, 
+        arvoreGeradoraMinima[i].destino, 
+        arvoreGeradoraMinima[i].custo);
+    }
+
+    printf("Custo total: %d\n", custoTotal);
+
+
+    free(arvoreGeradoraMinima);
+    liberarUnionFind(&uf);
 }
 
 
@@ -37,7 +103,7 @@ int main(){
     int quantidadeDeVertices = 0;   	                    
     int quantidadeDeArestas  = 0; 		                    	                			           
     char caminhoDoArquivo[50];
-    struct aresta *listaDeArestas = NULL;
+    struct aresta *grafo = NULL;
     
 
     FILE *arquivo = NULL;
@@ -56,21 +122,21 @@ int main(){
     //Lê o arquivo para capturar a quantidade de vertices e a quantidade de arestas e armazená-las em suas respectivas variáveis
     fscanf(arquivo, "%d %d", &quantidadeDeVertices, &quantidadeDeArestas); 
     
-    listaDeArestas = malloc(sizeof(struct aresta) * quantidadeDeArestas);
+    grafo = malloc(sizeof(struct aresta) * quantidadeDeArestas);
 
     //Lê o arquivo para capturar as arestas e armazená-las na lista
     for (int i = 0; i < quantidadeDeArestas; i++){
         fscanf(arquivo, "%d %d %d", 
-        &listaDeArestas[i].origem, 
-        &listaDeArestas[i].destino, 
-        &listaDeArestas[i].custo);
+        &grafo[i].origem, 
+        &grafo[i].destino, 
+        &grafo[i].custo);
     }
 
     fclose(arquivo);
 
-    kruskal(quantidadeDeVertices, quantidadeDeArestas, listaDeArestas);
+    kruskal(quantidadeDeVertices, quantidadeDeArestas, grafo);
 
-    free(listaDeArestas);
+    free(grafo);
 
     return 0;
 }
